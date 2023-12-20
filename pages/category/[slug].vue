6d8d1d40712ec090e3fc834d14be0729d1697d4d
yanuar <yanuaraditia@outlook.com>
2023-12-20 18:23:51 +0700

    Fix text size on some help page

--- pages/category/[slug].vue
+++ pages/category/[slug].vue
@@ -3,11 +3,11 @@
     <div class="prose prose-primary md:col-span-7 my-4 dark:prose-invert max-w-none">
       <div class="text-primary mb-3 font-medium">Knowledge Center</div>
       <div class="mb-3">
-        <h1 id="province" class="not-prose mb-3 text-xl md:text-5xl font-semibold" v-text="category.title"></h1>
+        <ProseH1 v-text="category.title"/>
         <p v-text="category.description"></p>
       </div>
       <div class="grid gap-2">
-        <div v-for="(article, key) in category.articles" :key="`key-${key}`" class="relative bg-surfaceContainerLow px-5 rounded-lg first:rounded-t-3xl last:rounded-b-3xl lg:flex py-3 md:py-4 hover:text-primary border-b-outlineVariant">
+        <div v-for="(article, key) in category.articles" :key="`key-${key}`" class="relative bg-surfaceContainerLow hover:bg-secondaryContainer px-5 rounded-lg first:rounded-t-3xl last:rounded-b-3xl lg:flex py-4 hover:text-primary border-b-outlineVariant">
           <h3 class="text-lg font-medium md:text-xl not-prose grow" v-text="article.fields.title"></h3>
           <span class="text-primary shrink-0 font-medium">Selengkapnya</span>
           <NuxtLink :to="`/article/${article.fields.slug}`" class="inset-0 absolute"/>
